cf273b7b443745268f1f2414dec235f4b88ae2d5
strike3test <strike3test@hotmail.com>
2020-05-16 22:04:45 +1200

    added winrm https port

--- main.tf
+++ main.tf
@@ -115,6 +115,12 @@ resource "aws_security_group" "allow_rdp" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 5986
+    to_port     = 5986
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
